a8bb15589f7c347fae4a58810aceb2fd597ab7d7
Mankirat Singh <106363094+mankirat657@users.noreply.github.com>
2026-07-09 18:30:46 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min  = Integer.MAX_VALUE;
+        for(int i = 0; i < prices.length;i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }else{
+                maxProfit = Math.max(maxProfit,Math.abs(min - prices[i]));
+            }
+        }
+
+        return maxProfit;
+    }
+}
